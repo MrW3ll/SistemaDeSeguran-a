b2ec1e9caea04937ba5fa30ad2c222ecfd50a4e8
Wellington Rodrigues de Paula Conceição <weel2803@outlook.com>
2025-03-27 06:37:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SistemaDeSegurança0._0._1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string? Login = "Admin";
+            string? Key = "123456";
+            string? User = null;
+            string? Password = null;
+
+            Console.WriteLine("Bem vindo! Deseja realizar Login:\nS(sim) N(não)");
+            char Opcão = char.Parse(Console.ReadLine());
+
+            switch(Opcão) {
+                case 'S':
+                    while(User != Login) {
+
+                        User = Console.ReadLine();
+
+                        if(User != null) {
+                            if(User == Login) {
+                                Console.WriteLine("Digite sua senha");
+                                Password = Console.ReadLine();
+                            } else {
+                                Console.WriteLine("Login não identificado");
+                            }
+                        } else {
+                            Console.WriteLine("Por favor digite o Login!");
+                        }
+                    }
+                break;
+                
+                case 'N':
+                    Console.WriteLine("Programa Finalizado");
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
